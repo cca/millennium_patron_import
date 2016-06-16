f4ec127ec50277f0789b834f2794ea915709d3a3
phette23 <phette23@gmail.com>
2016-06-16 12:21:37 -0700

    Fall 2016 updates to PCODE3 mapping

--- mapping.py
+++ mapping.py
@@ -7,7 +7,6 @@ ptype = {
 
 # See Millennium Circulation > Admin > Parameters > Circulation > Pcode 3
 # 3-digit, 000 to 255
-# ?s = VCS, MFA Comics, MAAD2&3
 pcode3 = {
     'ANIMA.BFA': '001',
     'ARCHT.BARC': '002',
@@ -15,24 +14,20 @@ pcode3 = {
     # combine with Design MBA which iniated the program
     'CIMBA.MBA': '020',
     'CERAM.BFA': '003',
+    'COMIC.MFA': '023',
     'COMAR.BFA': '004',
     'CURPR.MA': '017',
-
-    # is combo Design MFA + Design Strategy MBA double degree, we put with Design
+    # combo Design MFA + Design Strategy MBA dual degree, we put with Design
     'DD2ST': '024',
     'DESGN.MFA': '024',
     'DESST.MBA': '020',
-
     # these DVC** "programs" are for dual-degree grad students
-    # since we capture at least one of their other degrees we can skip them
-    # documented here just to avoid future confusion
-    #'DVCCP': '   ',  # Dual Degree Vis.Crit/Curatorial Practice
-    #'DVCFA': '   ',  # Dual Degree Vis. Crit/Fine Arts
-    #'DVCWR': '   ',  # Dual Degree Vis. Crit/Writing
-
+    # we arbitrarily choose to capture the non-VCS side of the degree for now
+    'DVCCP': '017',  # Dual Degree Vis. Crit/Curatorial Practice
+    'DVCFA': '026',  # Dual Degree Vis. Crit/Fine Arts
+    'DVCWR': '027',  # Dual Degree Vis. Crit/Writing
     #'EXTED': '   ',  # extended, leave null
     'FASHN.BFA': '005',
-
     'FCERM.MFA': '026',  # a few Fine Art MFAs share codes with their BFAs
     'FDRPT.MFA': '026',  # Drawing / Painting
     'FGLAS.MFA': '026',
@@ -47,12 +42,11 @@ pcode3 = {
     'FSCUL.MFA': '026',
     'FSOCP.MFA': '026',  # social practice, lump into MFA Fine Arts
     'FTEXT.MFA': '026',
-
     'GLASS.BFA': '008',
     'GRAPH.BFA': '009',  # if a few places we have BFA & MFA programs
     'GRAPH.MFA': '009',  # under the same PCODE number
     'ILLUS.BFA': '010',
-    'INACT.MFA': '024',  # plain ol' Design MFA, not Interactive Design
+    'INACT.MFA': '024',  # plain ol' Design MFA, not Interaction Design
     'INDIV.BFA': '011',
     'INDUS.BFA': '012',
     'INDUS.MFA': '012',
@@ -61,11 +55,11 @@ pcode3 = {
     # classify with Interaction Design BFA
     'IXDGR.MDES': '013',
     'IXDSN.BFA': '013',
-
     'MAAD1.MAAD': '019',
+    'MAAD2.MAAD': '019',
+    'MAAD3.MAAD': '019',
     'MARC2.MARC': '016',
     'MARC3.MARC': '016',
-
     'METAL.BFA': '015',
     #'NODEG.UG': '   ',  # no degree, leave null
     'PHOTO.BFA': '029',
@@ -74,7 +68,8 @@ pcode3 = {
     #'PRECO': '   ',  # leave null, pre-college
     'PRINT.BFA': '030',
     'SCULP.BFA': '031',
-    'SFMBA.MBA': '022',  # Strategic Finance MBA
+    'SFMBA.MBA': '022',  # Strategic Foresight MBA
+    'SOCPR.MA': '036',  # Social Practice MA, new Fall 2016
     'TEXTL.BFA': '032',
     'UNDEC.BFA': '033',
     'VISCR.MA': '018',
